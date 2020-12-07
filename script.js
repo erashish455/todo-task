5422bcb079825972212ae26f1ff10d123d0e7bab
ashish kumar kashyap <er.ashish455@gmail.com>
2020-12-07 19:35:39 +0530

    edit button implementation(not working)

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var parent = document.getElementById('list');
-
 function addList(value) {
     let a = document.getElementById('input').value
     if (a == '' && value == 'a') {
@@ -10,17 +8,37 @@ function addList(value) {
             b = ''
             document.getElementById('input').value = ''
         } else {
+            var parent = document.getElementById('list');
             let child = document.createElement('li')
             var button = document.createElement('button')
+            var button2 = document.createElement('button')
+
+            button.setAttribute('class', 'btn btn-outline-danger')
+            button2.setAttribute('class', 'btn btn-outline-danger')
+
             child.innerHTML = a
             button.innerHTML = 'X'
-            button.setAttribute('class', 'btn btn-outline-danger')
-            child.append(button)
-                //button.setAttribute('onClick', "this.parentElement.style.display = 'none';")
-            button.setAttribute('onClick', "this.parentNode.parentNode.removeChild(this.parentNode);")
+            button2.innerHTML = 'edit'
+            child.append(button, button2)
             parent.append(child)
+            button.setAttribute('onClick', "this.parentNode.parentNode.removeChild(this.parentNode);")
+            button2.setAttribute('onClick', "edit('this')")
             document.getElementById('input').value = null
         }
 
     }
 }
+
+function edit(e) {
+    console.log('andar aaya')
+    var temp = this.parent
+    console.log(temp)
+        // var temp2 = prompt(temp)
+        // this.innerHTML = temp2
+
+}
+
+function swap() {
+    document.
+}
+document.getElementById('.notDone li').addEventListener('onclick', swap())
